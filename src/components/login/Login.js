4945b56012b141a5b3c9e6c7f0c983b1056634d2
Francoveliz <francoleandroveliz@gmail.com>
2021-05-07 21:53:25 -0300

    api test

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
 	const getToken = async () => {
 		try {
 			const response = await axios.post(
-				"http://challenge-react.alkemy.org/",
+				"/challenge-react.alkemy.org.php/",
 				{
 					email: emailInputText,
 					password: passwordInputText,
